a2120ca17be0f946e52e4c54c3a72e5b977ebdf6
Shawn-Hanclock-Home <nwahs909@gmail.com>
2023-10-08 21:28:06 -0700

    Buttons change when clicked, now if the players win a toast is displayed and it's clear they won.

--- app/src/main/java/com/example/tictactoe/PlayActivity.java
+++ app/src/main/java/com/example/tictactoe/PlayActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.util.Arrays;
 
 public class PlayActivity extends AppCompatActivity {
     private Button toHomeButton;
@@ -17,7 +20,10 @@ public class PlayActivity extends AppCompatActivity {
             buttonML, buttonMC, buttonMR,
             buttonBL, buttonBC, buttonBR;
     private int[][] grid;
-    private boolean player1Turn = true;
+    private int turnNum = 1;
+    private int winsX = 0;
+    private int winsO = 0;
+    private TextView currentPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +44,18 @@ public class PlayActivity extends AppCompatActivity {
         });
 
         //declare button for all grid locations
-        buttonTL = (Button) findViewById(R.id.buttonTopL); //TopLeft
-        buttonTC = (Button) findViewById(R.id.buttonTopC); //TopCenter
-        buttonTR = (Button) findViewById(R.id.buttonTopR); //TopRight
-        buttonML = (Button) findViewById(R.id.buttonMidL); //MidLeft
-        buttonMC = (Button) findViewById(R.id.buttonMidC); //MidCenter
-        buttonMR = (Button) findViewById(R.id.buttonMidR); //MidRight
-        buttonBL = (Button) findViewById(R.id.buttonBotL); //BotLeft
-        buttonBC = (Button) findViewById(R.id.buttonBotC); //BotCenter
-        buttonBR = (Button) findViewById(R.id.buttonBotR); //BotRight
+        buttonTL = (Button) findViewById(R.id.btn0); //TopLeft
+        buttonTC = (Button) findViewById(R.id.btn1); //TopCenter
+        buttonTR = (Button) findViewById(R.id.btn2); //TopRight
+        buttonML = (Button) findViewById(R.id.btn3); //MidLeft
+        buttonMC = (Button) findViewById(R.id.btn4); //MidCenter
+        buttonMR = (Button) findViewById(R.id.btn5); //MidRight
+        buttonBL = (Button) findViewById(R.id.btn6); //BotLeft
+        buttonBC = (Button) findViewById(R.id.btn7); //BotCenter
+        buttonBR = (Button) findViewById(R.id.btn8); //BotRight
+
+        //current palyer declaration
+        currentPlayer = (TextView)findViewById(R.id.currentPlayer);
 
         //onclick listeners
         allOnCLicks();
@@ -64,9 +73,9 @@ public class PlayActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(grid[0][0] == 0) {
-                    if (player1Turn)
+                    if (turnNum % 2 != 0)
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[0][0] = 1;
                         buttonTL.setText("X");
 //                        Toast.makeText(PlayActivity.this,
@@ -74,18 +83,24 @@ public class PlayActivity extends AppCompatActivity {
                     }
                     else
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[0][0] = -1;
                         buttonTL.setText("O");
 //                        Toast.makeText(PlayActivity.this,
 //                                grid[0][0] + " value inside grid", Toast.LENGTH_LONG).show();
                     }
+                    currentPlayer.setText(turnNum %2 == 0 ? "Player TWO": "Player ONE");
                 }
                 else
                 {
                     CharSequence text = "Spot's Taken";
                     Toast.makeText(PlayActivity.this,
                             text, Toast.LENGTH_LONG).show();
+                }
+                if(turnNum > 5) {
+                    reset(winCheckCL(turnNum, grid)); //checks win
+                    reset(winCheckDL(turnNum, grid)); //checks win
+                    reset(winCheckRT(turnNum, grid)); //checks win
                 }
             }
         });
@@ -94,9 +109,9 @@ public class PlayActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(grid[0][1] == 0) {
-                    if (player1Turn)
+                    if (turnNum % 2 != 0)
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[0][1] = 1;
                         buttonTC.setText("X");
 //                        Toast.makeText(PlayActivity.this,
@@ -104,18 +119,23 @@ public class PlayActivity extends AppCompatActivity {
                     }
                     else
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[0][1] = -1;
                         buttonTC.setText("O");
 //                        Toast.makeText(PlayActivity.this,
 //                                grid[0][0] + " value inside grid", Toast.LENGTH_LONG).show();
                     }
+                    currentPlayer.setText(turnNum %2 == 0 ? "Player TWO": "Player ONE");
                 }
                 else
                 {
                     CharSequence text = "Spot's Taken";
                     Toast.makeText(PlayActivity.this,
                             text, Toast.LENGTH_LONG).show();
+                }
+                if(turnNum > 5) {
+                    reset(winCheckCC(turnNum, grid)); //checks win
+                    reset(winCheckRT(turnNum, grid)); //checks win
                 }
             }
         });
@@ -124,9 +144,9 @@ public class PlayActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(grid[0][2] == 0) {
-                    if (player1Turn)
+                    if (turnNum % 2 != 0)
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[0][2] = 1;
                         buttonTR.setText("X");
 //                        Toast.makeText(PlayActivity.this,
@@ -134,18 +154,24 @@ public class PlayActivity extends AppCompatActivity {
                     }
                     else
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[0][2] = -1;
                         buttonTR.setText("O");
 //                        Toast.makeText(PlayActivity.this,
 //                                grid[0][0] + " value inside grid", Toast.LENGTH_LONG).show();
                     }
+                    currentPlayer.setText(turnNum %2 == 0 ? "Player TWO": "Player ONE");
                 }
                 else
                 {
                     CharSequence text = "Spot's Taken";
                     Toast.makeText(PlayActivity.this,
                             text, Toast.LENGTH_LONG).show();
+                }
+                if(turnNum > 5) {
+                    reset(winCheckCR(turnNum, grid)); //checks win
+                    reset(winCheckDR(turnNum, grid)); //checks win
+                    reset(winCheckRT(turnNum, grid)); //checks win
                 }
             }
         });
@@ -154,9 +180,9 @@ public class PlayActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(grid[1][0] == 0) {
-                    if (player1Turn)
+                    if (turnNum % 2 != 0)
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[1][0] = 1;
                         buttonML.setText("X");
 //                        Toast.makeText(PlayActivity.this,
@@ -164,18 +190,23 @@ public class PlayActivity extends AppCompatActivity {
                     }
                     else
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[1][0] = -1;
                         buttonML.setText("O");
 //                        Toast.makeText(PlayActivity.this,
 //                                grid[0][0] + " value inside grid", Toast.LENGTH_LONG).show();
                     }
+                    currentPlayer.setText(turnNum %2 == 0 ? "Player TWO": "Player ONE");
                 }
                 else
                 {
                     CharSequence text = "Spot's Taken";
                     Toast.makeText(PlayActivity.this,
                             text, Toast.LENGTH_LONG).show();
+                }
+                if(turnNum > 5) {
+                    reset(winCheckCL(turnNum, grid)); //checks win
+                    reset(winCheckRM(turnNum, grid)); //checks win
                 }
             }
         });
@@ -184,9 +215,9 @@ public class PlayActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(grid[1][1] == 0) {
-                    if (player1Turn)
+                    if (turnNum % 2 != 0)
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[1][1] = 1;
                         buttonMC.setText("X");
 //                        Toast.makeText(PlayActivity.this,
@@ -194,18 +225,25 @@ public class PlayActivity extends AppCompatActivity {
                     }
                     else
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[1][1] = -1;
                         buttonMC.setText("O");
 //                        Toast.makeText(PlayActivity.this,
 //                                grid[0][0] + " value inside grid", Toast.LENGTH_LONG).show();
                     }
+                    currentPlayer.setText(turnNum %2 == 0 ? "Player TWO": "Player ONE");
                 }
                 else
                 {
                     CharSequence text = "Spot's Taken";
                     Toast.makeText(PlayActivity.this,
                             text, Toast.LENGTH_LONG).show();
+                }
+                if(turnNum > 5) {
+                    reset(winCheckCC(turnNum, grid)); //checks win
+                    reset(winCheckDL(turnNum, grid)); //checks win
+                    reset(winCheckDR(turnNum, grid)); //checks win
+                    reset(winCheckRM(turnNum, grid)); //checks win
                 }
             }
         });
@@ -213,9 +251,9 @@ public class PlayActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(grid[1][2] == 0) {
-                    if (player1Turn)
+                    if (turnNum % 2 != 0)
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[1][2] = 1;
                         buttonMR.setText("X");
 //                        Toast.makeText(PlayActivity.this,
@@ -223,18 +261,23 @@ public class PlayActivity extends AppCompatActivity {
                     }
                     else
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[1][2] = -1;
                         buttonMR.setText("O");
 //                        Toast.makeText(PlayActivity.this,
 //                                grid[0][0] + " value inside grid", Toast.LENGTH_LONG).show();
                     }
+                    currentPlayer.setText(turnNum %2 == 0 ? "Player TWO": "Player ONE");
                 }
                 else
                 {
                     CharSequence text = "Spot's Taken";
                     Toast.makeText(PlayActivity.this,
                             text, Toast.LENGTH_LONG).show();
+                }
+                if(turnNum > 5) {
+                    reset(winCheckCR(turnNum, grid)); //checks win
+                    reset(winCheckRM(turnNum, grid)); //checks win
                 }
             }
         });
@@ -242,9 +285,9 @@ public class PlayActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(grid[2][0] == 0) {
-                    if (player1Turn)
+                    if (turnNum % 2 != 0)
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[2][0] = 1;
                         buttonBL.setText("X");
 //                        Toast.makeText(PlayActivity.this,
@@ -252,18 +295,24 @@ public class PlayActivity extends AppCompatActivity {
                     }
                     else
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[2][0] = -1;
                         buttonBL.setText("O");
 //                        Toast.makeText(PlayActivity.this,
 //                                grid[0][0] + " value inside grid", Toast.LENGTH_LONG).show();
                     }
+                    currentPlayer.setText(turnNum %2 == 0 ? "Player TWO": "Player ONE");
                 }
                 else
                 {
                     CharSequence text = "Spot's Taken";
                     Toast.makeText(PlayActivity.this,
                             text, Toast.LENGTH_LONG).show();
+                }
+                if(turnNum > 5) {
+                    reset(winCheckCL(turnNum, grid)); //checks win
+                    reset(winCheckDR(turnNum, grid)); //checks win
+                    reset(winCheckRB(turnNum, grid)); //checks win
                 }
             }
         });
@@ -271,9 +320,9 @@ public class PlayActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(grid[2][1] == 0) {
-                    if (player1Turn)
+                    if (turnNum % 2 != 0)
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[2][1] = 1;
                         buttonBC.setText("X");
 //                        Toast.makeText(PlayActivity.this,
@@ -281,18 +330,23 @@ public class PlayActivity extends AppCompatActivity {
                     }
                     else
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[2][1] = -1;
                         buttonBC.setText("O");
 //                        Toast.makeText(PlayActivity.this,
 //                                grid[0][0] + " value inside grid", Toast.LENGTH_LONG).show();
                     }
+                    currentPlayer.setText(turnNum %2 == 0 ? "Player TWO": "Player ONE");
                 }
                 else
                 {
                     CharSequence text = "Spot's Taken";
                     Toast.makeText(PlayActivity.this,
                             text, Toast.LENGTH_LONG).show();
+                }
+                if(turnNum > 5) {
+                    reset(winCheckCC(turnNum, grid)); //checks win
+                    reset(winCheckRB(turnNum, grid)); //checks win
                 }
             }
         });
@@ -300,9 +354,9 @@ public class PlayActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(grid[2][2] == 0) {
-                    if (player1Turn)
+                    if (turnNum % 2 != 0)
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[2][2] = 1;
                         buttonBR.setText("X");
 //                        Toast.makeText(PlayActivity.this,
@@ -310,12 +364,13 @@ public class PlayActivity extends AppCompatActivity {
                     }
                     else
                     {
-                        player1Turn = !player1Turn;
+                        turnNum++;
                         grid[2][2] = -1;
                         buttonBR.setText("O");
 //                        Toast.makeText(PlayActivity.this,
 //                                grid[0][0] + " value inside grid", Toast.LENGTH_LONG).show();
                     }
+                    currentPlayer.setText(turnNum %2 == 0 ? "Player TWO": "Player ONE");
                 }
                 else
                 {
@@ -323,7 +378,89 @@ public class PlayActivity extends AppCompatActivity {
                     Toast.makeText(PlayActivity.this,
                             text, Toast.LENGTH_LONG).show();
                 }
+                if(turnNum > 5) {
+                    reset(winCheckCR(turnNum, grid)); //checks win
+                    reset(winCheckDL(turnNum, grid)); //checks win
+                    reset(winCheckRB(turnNum, grid)); //checks win
+                }
             }
         });
+    }
+
+    //win check for digaonal l to r
+    private int winCheckDL(int turns, int[][] nums)
+    {
+        return nums[0][0] + nums[1][1] + nums[2][2];
+    }
+    //win check for digaonal r to l
+    private int winCheckDR(int turns, int[][] nums)
+    {
+        return nums[2][0] + nums[1][1] + nums[0][2];
+    }
+    //win check for column l
+    private int winCheckCL(int turns, int[][] nums)
+    {
+        return nums[0][0] + nums[1][0]+ nums[2][0];
+    }
+    //win check for column c
+    private int winCheckCC(int turns, int[][] nums)
+    {
+        return nums[0][1] + nums[1][1] + nums[2][1];
+    }
+    //win check for column r
+    private int winCheckCR(int turns, int[][] nums)
+    {
+        return nums[0][2] + nums[1][2] + nums[2][2];
+    }
+    //win check for row t
+    private int winCheckRT(int turns, int[][] nums)
+    {
+        return nums[0][0] + nums[0][1] + nums[0][2];
+    }
+    //win check for row m
+    private int winCheckRM(int turns, int[][] nums)
+    {
+        return nums[1][0] + nums[1][1] + nums[1][2];
+    }
+    //win check for row b
+    private int winCheckRB(int turns, int[][] nums)
+    {
+        return nums[2][0] + nums[2][1] + nums[2][2];
+    }
+
+    private void reset(int score)
+    {
+        if(score == 3)
+        {
+            currentPlayer.setText("Player ONE");
+            for (int[] i : grid) {
+                Arrays.fill(i, 0);
+            }
+            winsX++;
+            CharSequence text = "Player 1: X Wins!";
+            Toast.makeText(PlayActivity.this,
+                    text, Toast.LENGTH_LONG).show();
+            TextView textView = (TextView)findViewById(R.id.totalXWin);
+            textView.setText(winsX + "");
+        }
+        else if(score == -3)
+        {
+            currentPlayer.setText("Player ONE");
+            for (int[] i : grid) {
+                Arrays.fill(i, 0);
+            }
+            winsO++;
+            CharSequence text = "Player 2: O Wins!";
+            Toast.makeText(PlayActivity.this,
+                    text, Toast.LENGTH_LONG).show();
+            TextView textView = (TextView)findViewById(R.id.totalOwin);
+            textView.setText(winsO + "");
+        }
+    }
+
+    private void resetButtons()
+    {
+         Button[] btn = new Button[9];
+//         String[] btn_id = {R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3};
     }
 }
