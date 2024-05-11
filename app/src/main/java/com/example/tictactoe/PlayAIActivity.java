5929a25a8f53378b2756b8c2d96541f320d14d41
Rico Embrado <embradorico@gmail.com>
2024-05-10 17:05:24 -0700

    Added one last navigation button so that you can go back home from the ai playing screen

--- app/src/main/java/com/example/tictactoe/PlayAIActivity.java
+++ app/src/main/java/com/example/tictactoe/PlayAIActivity.java
@@ -1,22 +1,34 @@
 package com.example.tictactoe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PlayAIActivity extends AppCompatActivity
 {
+    private Button backToHomeScreen;
 //    private final List<Button> BUTTONS = new ArrayList<>();
 //    private final int[] grid = new int[9];
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_ai);
 
+        backToHomeScreen = findViewById(R.id.backHome);
 //        Log.d("TAG", "Static call to check column " + PlayActivity.checkColumnSum(0));
 //        BUTTONS.addAll(Arrays.asList());
 //        for (Button button : BUTTONS)
 //            button.setOnClickListener(this);
     }
 
+    public void goHome(View v)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        finish();
+        this.startActivity(intent);
+    }
 }
